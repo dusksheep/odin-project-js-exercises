b59b5fa0a19dff64478864fc3d4ec62cfeaa73a7
thunderSheep <acostarenan@gmail.com>
2021-01-19 00:26:07 -0300

    fundamentals part 4 done

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,10 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+  let tempC = (tempF - 32) / 1.8;
+  let tempCRounded = Math.round(tempC * 10) / 10
+  return tempCRounded;
 }
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+  let tempF = (tempC * 1.8) + 32;
+  let tempFRounded = Math.round(tempF * 10) / 10
+  return tempFRounded;
 }
+
 
 module.exports = {
   ftoc,
