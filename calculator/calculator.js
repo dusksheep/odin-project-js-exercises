f332c969db21010c9717d64d2a637cd4f750e211
thunderSheep <acostarenan@gmail.com>
2021-01-23 01:39:54 -0300

    calculator and palindromes exercises

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,26 +1,62 @@
-function add () {
-	
+function add (number1, number2) {
+	return number1 + number2;
 }
 
-function subtract () {
-	
+function subtract (number1, number2) {
+	return number1 - number2;
 }
 
-function sum () {
-	
+function sum (array) {
+	let sum = 0;
+	let i = 0;
+	for(i = 0; i < array.length; i++) {
+		sum = sum + array[i];
+	}
+	return sum;
 }
 
-function multiply () {
+function multiply (arr) {
+	let i = 0;
+	let product = 1;
+	for(i = 0; i < arr.length; i++) {
+		product = product * arr[i]; 
 	
+	}
+	return product;
+
 }
 
-function power() {
-	
+
+function power(number1, number2) {
+	return number1 ** number2;
 }
 
-function factorial() {
+function factorial(number) {
+	let i = number
+	if(i === 0) {
+		return 1;
+	}
 	
-}
+	if(i === 1) {
+		return 1;
+	}
+	i--;
+	let factorial = number * (number-i);
+		
+	
+
+	
+	while(i > 1) {
+		i--;
+		factorial = factorial * (number - i);
+	}
+	console.log(factorial);
+	return factorial;
+	}
+
+	
+
+	
 
 module.exports = {
 	add,
--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function() {
-
+const palindromes = function(string) {
+let concatenatedString = string.toLowerCase().replace(/ /g, "");
+let processedString = concatenatedString.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+let reversedString = "";
+for(let i = 0; i < processedString.length; i++) {
+  reversedString = reversedString + processedString.charAt((processedString.length -1) - i);
 }
+console.log(reversedString);
+if(processedString === reversedString) {
+  return true;
+}
+return false;
+}
+
 
 module.exports = palindromes
